e00a0bbec67cdb39625c7ef45a14efd204ef3268
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-10 13:10:33 +0200

    Make tests pass with /_external-auth* path. Closes #4

--- test/liquid_voting_auth_web/controllers/token_controller_test.exs
+++ test/liquid_voting_auth_web/controllers/token_controller_test.exs
@@ -2,11 +2,11 @@ defmodule LiquidVotingAuthWeb.TokenControllerTest do
   use LiquidVotingAuthWeb.ConnCase
 
   describe "authorization header includes our demo token" do
-    test "GET /", %{conn: conn} do
+    test "GET /_external-auth*", %{conn: conn} do
       token = "1nHSXC7/hJIO0yjjclx2TnxrtofcXCT+iBl2M7p2uThWNIRnsBxIqurrr66Vr22h"
       conn = conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> token)
-        |> get("/")
+        |> get("/_external-auth")
 
       assert conn.status == 200
     end
