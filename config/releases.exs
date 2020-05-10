0cf0b59dc21e87498d8db1a133393141c1c29c04
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-10 13:44:38 +0200

    Enable db on releases

--- config/releases.exs
+++ config/releases.exs
@@ -1,20 +1,20 @@
 import Config
 
-# db_user = System.fetch_env!("DB_USERNAME")
-# db_password = System.fetch_env!("DB_PASSWORD")
-# db_name= System.fetch_env!("DB_NAME")
-# db_host= System.fetch_env!("DB_HOST")
-# db_pool_size = System.get_env("DB_POOL_SIZE") || "10"
+db_user = System.fetch_env!("DB_USERNAME")
+db_password = System.fetch_env!("DB_PASSWORD")
+db_name= System.fetch_env!("DB_NAME")
+db_host= System.fetch_env!("DB_HOST")
+db_pool_size = System.get_env("DB_POOL_SIZE") || "10"
 
 port = System.get_env("APP_PORT") || "4000"
 secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
 
-# config :liquid_voting_auth, LiquidVotingAuth.Repo,
-#   username: db_user,
-#   password: db_password,
-#   database: db_name,
-#   hostname: db_host,
-#   pool_size: String.to_integer(db_pool_size)
+config :liquid_voting_auth, LiquidVotingAuth.Repo,
+  username: db_user,
+  password: db_password,
+  database: db_name,
+  hostname: db_host,
+  pool_size: String.to_integer(db_pool_size)
 
 config :liquid_voting_auth, LiquidVotingAuthWeb.Endpoint,
   http: [:inet6, port: String.to_integer(port)],
@@ -30,9 +30,4 @@ config :liquid_voting_auth, LiquidVotingAuthWeb.Endpoint,
 # $ DB_PASSWORD=postgres \
 # $ DB_NAME=liquid_voting_auth_dev \
 # $ DB_HOST=localhost \
-# $ _build/prod/rel/liquid_voting_auth/bin/liquid_voting_auth start
-# $
-# Without database (current)
-# $
-# $ SECRET_KEY_BASE=$(mix phx.gen.secret) \
 # $ _build/prod/rel/liquid_voting_auth/bin/liquid_voting_auth start
