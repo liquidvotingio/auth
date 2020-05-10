583d5f5c4224657552a7af8d6031945aa18d0b73
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-10 13:25:10 +0200

    Rename demo token -> demo auth key

--- lib/liquid_voting_auth_web/controllers/auth_controller.ex
+++ lib/liquid_voting_auth_web/controllers/auth_controller.ex
@@ -1,11 +1,11 @@
 defmodule LiquidVotingAuthWeb.AuthController do
   use LiquidVotingAuthWeb, :controller
 
-  @demo_token "1nHSXC7/hJIO0yjjclx2TnxrtofcXCT+iBl2M7p2uThWNIRnsBxIqurrr66Vr22h"
+  @demo_auth_key "1nHSXC7/hJIO0yjjclx2TnxrtofcXCT+iBl2M7p2uThWNIRnsBxIqurrr66Vr22h"
 
   def index(conn, _params) do
     case get_req_header(conn, "authorization") do
-      ["Bearer " <> @demo_token] ->
+      ["Bearer " <> @demo_auth_key] ->
         conn |> send_resp(200, "")
       _ ->
         conn |> send_resp(401, "")
