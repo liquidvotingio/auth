10d7dc79d1627f2259fa3b47527042d4f3f5c52c
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-13 19:26:15 +0200

    Check for organization auth keys on AuthController. Closes #12

--- test/liquid_voting_auth_web/controllers/auth_controller_test.exs
+++ test/liquid_voting_auth_web/controllers/auth_controller_test.exs
@@ -1,9 +1,12 @@
 defmodule LiquidVotingAuthWeb.AuthControllerTest do
   use LiquidVotingAuthWeb.ConnCase
 
-  describe "authorization header includes our demo auth key" do
+  alias LiquidVotingAuth.Organizations
+
+  describe "authorization header includes a registered auth key for an organization" do
     test "GET /_external-auth*", %{conn: conn} do
-      auth_key = "1nHSXC7/hJIO0yjjclx2TnxrtofcXCT+iBl2M7p2uThWNIRnsBxIqurrr66Vr22h"
+      {:ok, organization} = Organizations.create_organization(%{name: "Democratic Circus"})
+      auth_key = organization.auth_key
       conn = conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> auth_key)
         |> get("/_external-auth")
@@ -21,9 +24,9 @@ defmodule LiquidVotingAuthWeb.AuthControllerTest do
     end
   end
 
-  describe "bad authorization header" do
+  describe "authorization header includes unregistered auth key" do
     test "GET /_external-auth*", %{conn: conn} do
-      auth_key = "bad auth key"
+      auth_key = Ecto.UUID.generate
       conn = conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> auth_key)
         |> get("/_external-auth")
