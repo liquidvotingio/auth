a142cc241c2607220283a7e3e505d417ccea8ac8
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-05-16 14:31:25 +0200

    Better descriptions for auth controller tests

--- test/liquid_voting_auth_web/controllers/auth_controller_test.exs
+++ test/liquid_voting_auth_web/controllers/auth_controller_test.exs
@@ -3,8 +3,8 @@ defmodule LiquidVotingAuthWeb.AuthControllerTest do
 
   alias LiquidVotingAuth.Organizations
 
-  describe "authorization header includes a registered auth key for an organization" do
-    test "GET /_external-auth*", %{conn: conn} do
+  describe "when authorization header includes a registered auth key for an organization" do
+    test "GET /_external-auth* responds with 200 OK", %{conn: conn} do
       {:ok, organization} = Organizations.create_organization(%{name: "Democratic Circus"})
       auth_key = organization.auth_key
       conn = conn
@@ -15,8 +15,8 @@ defmodule LiquidVotingAuthWeb.AuthControllerTest do
     end
   end
 
-  describe "no authorization header" do
-    test "GET /_external-auth*", %{conn: conn} do
+  describe "when no authorization header" do
+    test "GET /_external-auth* responds with 401 Unauthorized", %{conn: conn} do
       conn = conn
         |> get("/_external-auth")
 
@@ -24,8 +24,8 @@ defmodule LiquidVotingAuthWeb.AuthControllerTest do
     end
   end
 
-  describe "authorization header includes unregistered auth key" do
-    test "GET /_external-auth*", %{conn: conn} do
+  describe "when authorization header includes unregistered auth key" do
+    test "GET /_external-auth* responds with 401 Unauthorized", %{conn: conn} do
       auth_key = Ecto.UUID.generate
       conn = conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> auth_key)
@@ -35,8 +35,8 @@ defmodule LiquidVotingAuthWeb.AuthControllerTest do
     end
   end
 
-  describe "authorization header includes auth key in bad format" do
-    test "GET /_external-auth*", %{conn: conn} do
+  describe "when authorization header includes auth key in bad format" do
+    test "GET /_external-auth* responds with 401 Unauthorized", %{conn: conn} do
       auth_key = "1nHSXC7/hJIO0yjjclx2TnxrtofcXCT+iBl2M7p2uThWNIRnsBxIqurrr66Vr22h"
       conn = conn
         |> Plug.Conn.put_req_header("authorization", "Bearer " <> auth_key)
