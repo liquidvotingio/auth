b4648ded87fcbb0eb824630cc2d4c46fc12bf1ee
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-06-02 14:42:02 +0200

    Use POSTGRES_HOST on test config, if available
    In trying to get the CI to pass

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :liquid_voting_auth, LiquidVotingAuth.Repo,
   username: "postgres",
   password: "postgres",
   database: "liquid_voting_auth_test",
-  hostname: "localhost",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
