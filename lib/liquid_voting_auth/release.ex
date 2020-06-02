e5def4c795a7b8a11bbab6bb9bed686e382fa188
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2020-06-02 13:12:57 +0200

    LiquidVotingAuth.Release.create_org/1
    For quick auth key creation while we don't have proper organization signup

--- lib/liquid_voting_auth/release.ex
+++ lib/liquid_voting_auth/release.ex
@@ -1,5 +1,27 @@
+# Release hooks that can be eval()'d at runtime
+#
+# Examples:
+#
+# mix release
+#
+# SECRET_KEY_BASE=$(mix phx.gen.secret) \
+# DB_USERNAME=postgres \
+# DB_PASSWORD=postgres \
+# DB_NAME=liquid_voting_auth_dev \
+# DB_HOST=localhost \
+# _build/dev/rel/liquid_voting_auth/bin/liquid_voting_auth eval 'LiquidVotingAuth.Release.migrate'
+#
+# SECRET_KEY_BASE=$(mix phx.gen.secret) \
+# DB_USERNAME=postgres \
+# DB_PASSWORD=postgres \
+# DB_NAME=liquid_voting_auth_dev \
+# DB_HOST=localhost \
+# _build/dev/rel/liquid_voting_auth/bin/liquid_voting_auth eval 'LiquidVotingAuth.Release.create_org("Bonafide Test Organization")'
+#
 defmodule LiquidVotingAuth.Release do
   @app :liquid_voting_auth
+
+  alias LiquidVotingAuth.Organizations.Organization
 
   def migrate do
     for repo <- repos() do
@@ -9,6 +31,20 @@ defmodule LiquidVotingAuth.Release do
 
   def rollback(repo, version) do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  def create_org(org_name) do
+    for repo <- repos() do
+      attrs = %{name: org_name}
+
+      {:ok, org, _} = Ecto.Migrator.with_repo(repo, fn repo ->
+        %Organization{}
+        |> Organization.changeset(attrs)
+        |> repo.insert!
+      end)
+      IO.puts "Organization #{org.name} created"
+      IO.puts "Auth key: #{org.auth_key}"
+    end
   end
 
   defp repos do
